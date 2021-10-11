227f966232d4915f8fa8819f3ce73117160c861e
maik.tizziani <maik.tizziani@edith-bahn.de>
2021-10-11 10:31:42 +0200

    add jsdocs to string functions

--- src/string.js
+++ src/string.js
@@ -1,3 +1,8 @@
+/**
+ * get a new string with removed whitespaces
+ * @param {string} input
+ * @return {string}
+ */
 export function removeAllWhitespaces(input) {
     let result = input;
     let regEx = /\s+/g;
@@ -5,6 +10,11 @@ export function removeAllWhitespaces(input) {
     return result;
 }
 
+/**
+ * get a new string without dashes
+ * @param {string} input
+ * @return {string}
+ */
 export function removeAllDashes(input) {
     let result = input;
     let regEx =/\-+/g;
@@ -12,6 +22,11 @@ export function removeAllDashes(input) {
     return result;
 }
 
+/**
+ * get a new string without double whitespaces
+ * @param {string}input
+ * @return {string}
+ */
 export function replaceAllDoubleWhitespacesWithSpace(input) {
     let result = input;
     let regEx = /\s\s+/g;
